02e57347e4258114b1137386213cf998e595060a
testGit <test@Git.com>
2023-08-14 14:49:30 +0530

    fixed the api data issue

---
+++ WeatherApp/Readme.md
@@ -0,0 +1,19 @@
+---WEATHER APP & ITS ARCHITECTURE---
+- This small project is to develop my understanding of API calls in a web application.
+- Information like temperature, wind speed and humidity are fetched and displayed to the user as per the user input
+- This project contains 3 files and script.js contains the API logic handling
+- The icons in the weather card are in the images folder
+- index.html has the basic template of the information which is then later managed by DOM.
+- style.css has all the properties to give the look and feel of the application.
+
+
+---LOGIC---
+- There are 2 API calls/urls being used from a free weather app (www.openweathermap.org)
+- apiUrl1 is for coordinate conversion of the user input city
+- apiUrl2 is for weather details to display to the user.
+
+
+---BUGS---
+- none so far
+
+
--- WeatherApp/script.js
+++ WeatherApp/script.js
@@ -1,37 +1,42 @@
 const apiKey = '9ba6eda27c85d260927aed5e8e0a5ac8';
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?&units=metric';
+const apiUrl1 = 'http://api.openweathermap.org/geo/1.0/direct?q="';
+const apiUrl2 = 'https://api.openweathermap.org/data/2.5/weather?'
 
 const searchBox = document.querySelector(".search input");
 const searchButton = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
-var lat = '40.7128';
-var lon = '-73.9352';
+async function checkWeather(city){
+    const coordinates = await fetch(apiUrl1 + city + `"&limit1=2&appid=${apiKey}`);
+    var data1 = await coordinates.json();
 
-var lon, lat;
+    console.log(data1);
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + `&lat=${lat}&lon=${lon}&appid=${apiKey}`);
-    var data = await response.json();
+    var longi = (data1[0].lon).toFixed(5);
+    var lati = (data1[0].lat).toFixed(5);
 
-    console.log(data);
+    const response2 = await fetch ( apiUrl2 + `lat=${lati}&lon=${longi}&appid=${apiKey}`)
+    var data2 = await response2.json()
 
-    document.querySelector('.city').innerHTML = data.name;
-    document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "°C";
-    document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
-    document.querySelector('.wind').innerHTML = data.wind.speed + " km/h";
+    console.log(data2);
 
-    if(data.weather[0].main == 'Clouds'){
+
+    document.querySelector('.city').innerHTML = data2.name;
+    document.querySelector('.temp').innerHTML = Math.round(data2.main.temp) + "°C";
+    document.querySelector('.humidity').innerHTML = data2.main.humidity + "%";
+    document.querySelector('.wind').innerHTML = data2.wind.speed + " km/h";
+
+    if(data2.weather[0].main == 'Clouds'){
         weatherIcon.src = "images/clouds.png";
-    } else if(data.weather[0].main == 'Clear'){
+    } else if(data2.weather[0].main == 'Clear'){
         weatherIcon.src = "images/clear.png";
-    } else if(data.weather[0].main == 'Rain'){
+    } else if(data2.weather[0].main == 'Rain'){
         weatherIcon.src = "images/rain.png";
-    } else if(data.weather[0].main == 'Snow'){
+    } else if(data2.weather[0].main == 'Snow'){
         weatherIcon.src = "images/snow.png";
-    } else if(data.weather[0].main == 'Drizzle'){
+    } else if(data2.weather[0].main == 'Drizzle'){
         weatherIcon.src = "images/drizzle.png";
-    } else if(data.weather[0].main == 'Mist'){
+    } else if(data2.weather[0].main == 'Mist'){
         weatherIcon.src = "images/mist.png";
     }
 
